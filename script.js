4d0ae4c6a81c6c3d0911f9e6826235969f36bd02
Tarakanov <anniktar11@gmail.com>
2017-05-15 23:22:14 +0500

    shipId-dataset-classShip

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 var tbodyPeople = document.getElementById("fieldPeople");
 var arrayFieldPeople = generateArray();
-var optionBattle = { };
+var optionBattle = {
+    oneCell : [4, 1], //count - size
+    twoCell : [3, 2],
+    threeCell : [2, 3],
+    fourCell : [1, 4]
+};
 
 "use strict";
+class Ship {
+    constructor(nameShip, size, x, y, direction) {
+        this.nameShip = nameShip;
+        this.size = size;
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    };
+    get coordinate() {
+        return [this.x, this.y, this.direction];
+    };
+    set coordinate(newCoord) {
+        [this.x, this.y, this.direction] = newCoord;
+    };
+    countCell(){
+        this.size -= 1;
+    };
+};
+
+let ship1 = new Ship ("cruiser", 4);
+ship1.coordinate = [1,2,3];
+
 function generateArray(){
     let state = [];
     for (let i=0; i<10; i++) {
         state[i] = [];
         for (let j=0; j<10; j++) {
             state[i].push({      //add end
-                value: 'ship',
+                value: 'empty',
                 x: j,
                 y: i
             })
@@ -18,50 +45,18 @@ function generateArray(){
     return state;
 };
 
-function Ship (nameShip, size){
-    var allCount = 20;
-    this.nameShip = nameShip;
-    this.size = size;
-    this.nowSize = size;
-    this.calcSize = function () {
-        this.nowSize -= 1;
-        return this.nowSize;
-    };
-};
-
-//Ship.countAll = function (){
-//   this.allCount -= 1;
-//  return this.allCount;
-//};
-
-var shipOne = new Ship('OnePaluba', 4);
-
-shipOne.calcSize();
-console.log(Ship.allCount);
-console.log(shipOne.allCount + ' : ' + shipOne.nowSize);
-shipOne.calcSize();
-console.log(shipOne.allCount + ' : ' + shipOne.nowSize);
-
-//-----------------------------------------
-function createObj () {
-
-};
-
-function createShip (){
-
-};
-
-
-
 function paintField(){ //replace in paintField arguments;
-
     tbodyPeople.innerHTML = arrayFieldPeople.map( function (row, rowId){
-            return '<tr>' + row.map( function (cell ,cellId){
-                return '<td '+'id="cell-'+rowId+':'+cellId +'"'+' class="cell cellValue-'+cell.value+'"' +'>'+rowId+':'+cellId+'</td>'
+        return '<tr>' + row.map( function (cell ,cellId){
+                return '<td '+'id="cell-'+rowId+':'+cellId +'"'+' class="cell cellValue-'+cell.value+'"'+' data-ship-id="'+cellId+'"'+'>'+rowId+':'+cellId+'</td>'
             }).join('') + '</tr>';
-        }).join('');
+    }).join('');
+};
 
+function lineDataShow(x, y, id) {
+    var cell = document.getElementById(id + [x,y].join(':'));
+    console.log ("LineDataShow = " + cell.dataset.shipId);
 };
 
 paintField();
-
+lineDataShow(0, 0, 'cell-');
