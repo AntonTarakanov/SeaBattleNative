7820cf0409b79a8bc81300fb6492ab1a8c3693ad
AntonTarakanov <anniktar11@gmail.com>
2017-05-26 01:03:28 +0500

    i'm working on this project. Will soon be divided into several files.

--- script.js
+++ script.js
@@ -26,9 +26,20 @@ class Ship {
     set coordinate(newCoord) {
         [this.x, this.y, this.direction] = newCoord;
     };
-    countCell(){
+    sizeReduce(){
         this.size -= 1;
     };
+};
+
+class Trial {
+  constructor (idShip, objShip, map) {
+      this.x = randomNumber(0,9);
+      this.y = randomNumber(0,9);
+      this.course = modifierCourse(randomNumber(0,1));
+      this.idShip = idShip;
+      this.objShip = objShip;
+      this.map = map;
+  };
 };
 
 function createMap(){
@@ -37,23 +48,14 @@ function createMap(){
         state[i] = [];
         for (let j=0; j<10; j++) {
             state[i].push({
-                value: 'empty',
                 x: j,
                 y: i,
-                countObj: undefined
+                value: 'empty',
+                idShip: undefined
             })
-        }
-    };
-    return state;
-};
-
-function createShip(array) {
-    for (let key in configBattle) {
-        for (let i=0; i<configBattle[key][0] ; i++) {
-            let ship = new Ship (key, configBattle[key][1]); //count-size
-            array.push(ship);
         };
     };
+    return state;
 };
 
 function randomNumber(min, max) { //including min-max
@@ -65,111 +67,119 @@ function modifierCourse(course){
     return "vertically";
 };
 
-function isInsideBox(coordinate, size){
-    let coord = coordinate+size;
-    return (coord >= 0) && (coord < 10);
+function getTrial (idShip, objShip, map){
+    let trial = new  Trial(idShip, objShip, map);
+    return trial;
 };
 
-function addMap (x, y, type, count, map){
-    map[x][y].value = type;
-    map[x][y].countObj = count;
+function createShip(array) {
+    for (let key in configBattle) {
+        for (let i=0; i<configBattle[key][0] ; i++) {
+            let ship = new Ship (key, configBattle[key][1]); //count-size
+            array.push(ship);
+        };
+    };
 };
 
-//-----------------------------installShips
-function installShips(array, map) {
-    for (let i=0; i<array.length; i++) {
+function installShips(arrayShips, map) {
+    for (let i=0; i<arrayShips.length; i++) {
         let put = false;
         do {
-            let offer = [randomNumber(0, 9), randomNumber(0, 9), modifierCourse(randomNumber(0, 1)), array[i], i];
-            if (inspectBorder(offer)) {
-                if (inspectLocation(offer, map)) {
-                    put = putShip(offer, map);
+            let trial = getTrial(i, arrayShips[i], map);
+            if (inspectBorder(trial)) {
+                if (inspectLocation(trial)) {
+                    put = putShip(trial);
                 };
             };
         } while (put === false);
     };
 };
 
-function putShip(offer, map) {
-    offer[3].coordinate = [offer[0],offer[1],offer[2]];
-    for (let i=0; i<offer[3].size; i++) {
-        if (offer[2] === "horizontally") {
-            addMap (offer[0], offer[1]+i, "ship", offer[4], map);
-        };
-        if (offer[2] === "vertically") {
-            addMap (offer[0]+i, offer[1], "ship", offer[4], map);
-        };
-    };
-    installRound(offer, map);
-    exampleArray(offer, map);
-    return true;
+function inspectBorder(trial) {
+    if (trial.course === "horizontally") return isInsideBox(trial.y, trial.objShip.size);
+    if (trial.course === "vertically") return isInsideBox(trial.x, trial.objShip.size);
 };
 
-function inspectBorder(offer) {
-    if (offer[2] === "horizontally") return isInsideBox(offer[1], offer[3].size);
-    if (offer[2] === "vertically") return isInsideBox(offer[0], offer[3].size);
+function isInsideBox(coordinate, size){
+    let coord = coordinate+size;
+    return (coord >= 0) && (coord < 10);
 };
 
-function inspectLocation(offer,map) {
-    for (let i=0; i<offer[3].size; i++) {
-        if (offer[2] === "horizontally") {
-            if ( map[offer[0]] [offer[1]+i].value != "empty") return false;
+function isInsideBoxTwo(x, y){
+    return (x >= 0) && (x < 10) && (y >= 0) && (y < 10);
+};
+
+function inspectLocation(trial) {
+    for (let i=0; i<trial.objShip.size; i++) {
+        if (trial.course === "horizontally") {
+            if ( trial.map[trial.x] [trial.y+i].value != "empty") return false;
         };
-        if (offer[2] === "vertically") {
-            if ( map[offer[0]+i] [offer[1]].value != "empty") return false;
+        if (trial.course === "vertically") {
+            if ( trial.map[trial.x+i] [trial.y].value != "empty") return false;
         };
     };
     return true;
 };
-//-----------------------------installShips
 
-//-------------------------------------------installRound
-
-function installRound(offer, map) {
-    if (offer[2] === "horizontally") installRoundHor(offer, map);
-    if (offer[2] === "vertically") installRoundVer(offer, map);
-};
-
-function installRoundHor(offer, map){
-    for (let i=-1; i<offer[3].size+1; i++){
-        if ((isInsideBox(offer[0]-1,0))&&(isInsideBox(offer[1]+i,0))) addMap(offer[0]-1,offer[1]+i,"round",undefined, map);
-        if ((isInsideBox(offer[0]+1,0))&&(isInsideBox(offer[1]+i,0))) addMap(offer[0]+1,offer[1]+i,"round",undefined, map);
+function putShip(trial) {
+    trial.objShip.coordinate = [trial.x,trial.y,trial.course];
+    for (let i=0; i<trial.objShip.size; i++) {
+        if (trial.course === "horizontally") {
+            addMap (trial, trial.x, trial.y+i, "ship");
+        };
+        if (trial.course === "vertically") {
+            addMap (trial, trial.x+i, trial.y, "ship");
+        };
     };
-    if ((isInsideBox(offer[0],0))&&(isInsideBox(offer[1]-1,0))) addMap(offer[0],offer[1]-1,"round",undefined, map);
-    if ((isInsideBox(offer[0],0))&&(isInsideBox(offer[1]+offer[3].size,0))) addMap(offer[0],offer[1]+offer[3].size,"round",undefined, map);
+    startRound(trial);
+    return true;
 };
 
-function installRoundVer(offer, map){
-    for (let i=-1; i<offer[3].size+1; i++){
-        if ( (isInsideBox(offer[0]+i,0))&&(isInsideBox(offer[1]-1,0))) addMap(offer[0]+i,offer[1]-1,"round",undefined, map);
-        if ( (isInsideBox(offer[0]+i,0))&&(isInsideBox(offer[1]+1,0))) addMap(offer[0]+i,offer[1]+1,"round",undefined, map);
+function addMap (trial, x, y, type){
+    trial.map[x][y].value = type;
+    trial.map[x][y].countObj = trial.idShip;
+};
+
+function startRound(trial) {
+    let arrayRound=[];
+    if (trial.course === "horizontally") {
+        for (let i=-1; i<trial.objShip.size+1; i++) {
+            arrayRound.push({x: trial.x + 1, y: trial.y + i}, {x: trial.x - 1, y: trial.y + i});
+        };
+        arrayRound.push( {x: trial.x, y: trial.y-1}, {x: trial.x, y: trial.y+trial.objShip.size});
     };
-    if ((isInsideBox(offer[0]-1,0))&&(isInsideBox(offer[1],0))) addMap(offer[0]-1,offer[1],"round",undefined, map);
-    if ((isInsideBox(offer[0]+offer[3].size,0))&&(isInsideBox(offer[1],0))) addMap(offer[0]+offer[3].size,offer[1],"round",undefined, map);
-};
-//-------------------------------------------installRound
-
-//-------------------------------------------click and dataset testing
-function hit(x,y){
-    updateCell(x,y,"hit","cell-");
-    console.log("hit - "+x+":"+y);
+    if (trial.course === "vertically") {
+        for (let i=-1; i<trial.objShip.size+1; i++) {
+            arrayRound.push({x: trial.x + i, y: trial.y + 1},{x: trial.x + i, y: trial.y - 1});
+        };
+        arrayRound.push( {x: trial.x-1, y: trial.y},{x: trial.x+trial.objShip.size, y: trial.y} );
+    };
+    installRound(arrayRound, trial.map);
 };
 
-function updateCell(x, y, value, id){
-    var cell = document.getElementById(id + [x, y].join(':'));
-    cell.className = 'cell cellValue-' + value;
-    console.log("updateCell");
+function installRound(array, map) {
+    for (let key in array) {
+        if (isInsideBoxTwo(array[key].x, array[key].y)) {
+            map[array[key].x][array[key].y].value = "round";
+        };
+    };
 };
-//-------------------------------------------click and dataset testing
-//-------------------------------------------paintField
+
 function paintField(array,field){
     field.innerHTML = array.map( function (row, rowId){
         return '<tr>' + row.map( function (cell ,cellId){
-                return '<td '+'id="cell-'+rowId+':'+cellId +'"'+' onclick="hit('+rowId+','+cellId +')" class="cell cellValue-'+cell.value+'"'+' data-ship-id="'+cell.countObj+'"'+'>'+rowId+':'+cellId+'</td>'
+                return '<td '+'id="cellPl-'+rowId+':'+cellId +'"'+' class="cell cellValue-'+cell.value+'"'+' data-ship-id="'+cell.countObj+'"'+'>'+rowId+':'+cellId+'</td>'
             }).join('') + '</tr>';
     }).join('');
 };
-//-------------------------------------------paintFields
+
+function paintFieldOpponent(array,field){
+    field.innerHTML = array.map( function (row, rowId){
+        return '<tr>' + row.map( function (cell ,cellId){
+                return '<td '+'id="cellOp-'+rowId+':'+cellId +'"'+' class="cell cellValue-'+"empty"+'"'+' data-ship-id="'+cell.countObj+'"'+'>'+rowId+':'+cellId+'</td>'
+            }).join('') + '</tr>';
+    }).join('');
+};
 
 createShip(shipsPlay);
 installShips(shipsPlay, mapPeople);
@@ -177,4 +187,107 @@ paintField(mapPeople, tbodyPeople);
 
 createShip(shipsOpponent);
 installShips(shipsOpponent, mapOpponent);
-paintField(mapOpponent, tbodyOpponent);
+paintFieldOpponent(mapOpponent, tbodyOpponent);
+
+opponent.addEventListener("click", shot);
+//-------------------------------------------------------------------------
+class OptionSS {
+    constructor(status) {
+        this.status = status;
+        this.xLastHit = undefined;
+        this.yLastHit = undefined;
+        this.course = undefined;
+        this.resultLast = undefined;
+        this.logShot = [];
+    };
+
+    /*get status() { return [this.status]; };
+    set status(bool) { this.status = bool; };*/
+
+    addLog(dataShot){ this.logShot.push(dataShot); };
+};
+let optionSS = new OptionSS(false);
+let arrayState = ["empty","kill","miss"];
+class ComputerShotData {
+    constructor() {
+        this.x = randomNumber(0,9);
+        this.y = randomNumber(0,9);
+    };
+};
+
+function getDataset(x,y){
+
+};
+
+function updateCell(x, y, value, id){
+    let cell = document.getElementById(id + [x, y].join(':'));
+  //  cell.className = 'cell cellValue-' + value;
+};
+
+function shot(event){
+    let shipId = event.target.dataset.shipId;
+    let idCell = event.target.id;
+    let divId = event.currentTarget.id;
+    console.log("dataSet="+shipId+"|. idCell="+idCell+ "|. divId = "+divId);
+};
+
+function randomFirstShot (){
+    if (randomNumber(0,1)===1) { shotPeople() }
+    else { shotOpponent() };
+};
+
+function shotPeople(){
+    console.log("your turn. Shoot!");
+};
+
+function shotOpponent(){
+    console.log("The first move of the computer!");
+    if (optionSS.status === false) randomShot();
+    if (optionSS.status === true) smartShot();
+};
+
+function randomShot(){
+    let indicator = false;
+    do {
+        let dataShot = new ComputerShotData();
+            if (inspectShot(dataShot)) {
+                doShot(dataShot);
+                indicator = true;
+            };
+    } while (indicator === false);
+};
+
+function inspectShot (dataShot) {
+    for (let key in arrayState){
+        if (mapPeople[dataShot.x][dataShot.y].value === arrayState[key]) return false;
+    };
+    return true;
+};
+
+function doShot(dataShot) {
+    if (mapPeople[dataShot.x][dataShot.y].value === "ship") {
+        updateCell(dataShot.x, dataShot.y, "hit", "cell-");
+        //changeObjShips ();
+    } else {
+        updateCell(dataShot.x, dataShot.y, "miss", "cell-");
+    };
+    //shipAnalysis();
+};
+
+function smartShot(){
+    console.log("smart shot");
+};
+
+function shipAnalysis(){
+    console.log("shipAnalysis");
+};
+
+function changeObjShip(){
+    console.log("changeObjShip");
+};
+/*function corrective (){
+
+};*/
+
+randomFirstShot();
+
